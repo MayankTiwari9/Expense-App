28cec0780c09d3d82eb3f4a8bc437f10c23a2623
MayankTiwari9 <mt1027319@gmail.com>
2023-11-13 23:33:53 +0530

    Dynamic Data

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,13 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
   return (
-    <div>
-      <h2>Expense item!</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 100</p>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <div>{props.LocationOfExpenditure}</div>
+      </div>
     </div>
   );
 }
